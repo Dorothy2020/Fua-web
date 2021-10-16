f7d053fa9b3f5aef1de14fc1f3b7289fc0cf39f9
Dorothy2020 <wangarraakoth@gmail.com>
2021-10-16 11:06:12 +0300

    mamafua

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+// see if file2 javascript work on concat()
+console.log("This is file2 ");
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('active');
+});
+
+console.log("HEY")
